9c9c3a2196a311bd514f4d092ce8a16ce5fb30fc
Dario Sadel VB <dar8900@gmail.com>
2023-03-17 15:33:32 +0100

    Aggiornato script con log level info

--- make_deb.py
+++ make_deb.py
@@ -3,8 +3,7 @@ import os
 import argparse
 import logging
 
-logging.getLogger().setLevel(logging.DEBUG)
-logging.basicConfig(format='%(asctime)s - [%(levelname)s] - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - [%(levelname)s] - %(message)s', level=logging.INFO)
 
 class DebPgk:
 	def __init__(self, binary_info : tuple, 
